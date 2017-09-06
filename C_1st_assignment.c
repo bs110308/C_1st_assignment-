15c6f35cb93aa56347bdb60973d4cffa0d3bf14b
bs110308 <31677714+bs110308@users.noreply.github.com>
2017-09-06 14:54:17 +0900

    Add files via upload

---
+++ C_1st_assignment.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+printf("your student id number=2017301087\n");
+
+printf("your name= ±è¹ü¼ö\n");
+
+printf("your mobile phone number=01091767085\n");
+
+printf("your GitHub address=beom_20\n");
+
+return 0;
+}
